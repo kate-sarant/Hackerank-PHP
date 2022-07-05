c347a02b3f611554c5ba11567216f8b2437a71ab
Katerina Sarantopoulou <72164559+kate-sarant@users.noreply.github.com>
2022-07-05 08:32:16 +0300

    Update findThePairs.php
    remove white spaces

--- findThePairs.php
+++ findThePairs.php
@@ -1,24 +1,9 @@
 <?php
-$ar = [
-  10,10
-,20,20
-,20,
-30,30,
-30,30,
-50,
-70,
-40,40,
-40,40,
-40];
-
-
-
+$ar = [10,10,20,20,20,30,30,30,30,50,70,40,40,40,40,40];
 $singles;
 $pairs;
 
-
 foreach (array_values(array_count_values($ar)) as $key => $value) {
-
     if($value == 1){
       $singles = $singles +1;
     }
@@ -29,14 +14,9 @@ foreach (array_values(array_count_values($ar)) as $key => $value) {
       else{
         if(($value % 2 !== 0) && $value !== 1){
               echo "<br>";
-
               $pairs = $pairs + (($value/2)%10);
               $singles =$singles+1;
-
-
         }
-
 }
 }
-
  ?>
